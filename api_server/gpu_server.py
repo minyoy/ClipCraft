c574f9fe735a14aeceb0658aca673a338cab76c6
sarin <holic702@gmail.com>
2026-04-29 19:47:54 +0900

    gpu server 포트번호 8001로 변경

--- api_server/gpu_server.py
+++ api_server/gpu_server.py
@@ -45,4 +45,4 @@ async def process_ai_logic(request: GPUProcessRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
